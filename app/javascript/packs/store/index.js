0d5c97ccc4f194860da932256c2239f779afedcf
jaykilleen <me@jaykilleen.com>
2020-03-05 23:20:59 +1000

    Found the bug that caused Vuex `this.$store` to be undefined.

--- app/javascript/packs/store/index.js
+++ app/javascript/packs/store/index.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
+import Vue from 'vue/dist/vue.esm'
 import Vuex from 'vuex'
 import posts from './modules/posts'
 
 Vue.use(Vuex)
+
 
 export default new Vuex.Store({
   modules: {
